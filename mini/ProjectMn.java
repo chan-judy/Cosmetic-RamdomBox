c69bd8e504a5265c1649d682bfaf3c52ea14af15
chan-judy <judys.git@gmail.com>
2022-06-22 21:25:55 +0900

    first commit

---
+++ mini/ProjectMn.java
@@ -0,0 +1,13 @@
+package com.pj.mini;
+
+public interface ProjectMn {
+	
+	public void write();
+	public void writeFile();
+	public int input() throws Exception;
+	public void print();
+	public void event();
+	public void ranran();
+	public void check();
+
+}
